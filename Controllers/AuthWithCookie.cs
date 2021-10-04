b26e828d02dc39ec04888395f076ff0e86e2583c
Bruno Henri Garcia Viterbo <bruno.viterbo@bs2tecnologia.com.br>
2021-10-04 17:59:19 -0300

    Initial

---
+++ Controllers/AuthWithCookie.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MultipleAuth.Controllers
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class AuthWithCookie : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> ValidarAuth()
+        {
+            var user = HttpContext.User;
+
+            return Ok($"You can access: { user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value }");
+        }
+
+        [HttpGet("Account/Login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            var userClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "MeuUser"),
+                new Claim(ClaimTypes.Email, "meu@email.com"),
+            };
+
+            var minhaIdentity = new ClaimsIdentity(userClaims, "Usuario");
+            var userPrincipal = new ClaimsPrincipal(new[] { minhaIdentity });
+
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+
+            return Ok("Do Login first");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MultipleAuth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => Configuration.Bind("JwtSettings", options))
+                    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+                    {
+                        options.Cookie.Name = "UserLoginCookie";
+                    });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
